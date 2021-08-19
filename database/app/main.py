b95e544d341d71b25bb1ee43e6d2f03793ccccd1
ltiinagn <lingtianmsnexplorer@msn.com>
2021-08-19 19:30:45 +0800

    #1 limit links to number of times

--- database/app/main.py
+++ database/app/main.py
@@ -7,7 +7,7 @@ def connect(database_folder_path, database_path):
 		con = sqlite3.connect(database_path, check_same_thread=False)
 		cur = con.cursor()
 		cur.execute('''CREATE TABLE urlShortener
-			(idx int, shortenedURL text, fullURL text)''')
+			(idx int, shortenedURL text, fullURL text, times int)''')
 	else:
 		con = sqlite3.connect(database_path, check_same_thread=False)
 	return con
@@ -15,8 +15,15 @@ def connect(database_folder_path, database_path):
 def findEntry(cur, column, value):
 	return cur.execute("SELECT * FROM urlShortener WHERE {} == '{}'".format(column, value)).fetchone()
 
+def findEntryNull(cur, column, value):
+	return cur.execute("SELECT * FROM urlShortener WHERE {} {}".format(column, value)).fetchall()
+
 def addEntry(con, cur, idx, shortenedURL, fullURL):
-	cur.execute("INSERT INTO urlShortener VALUES ({},'{}','{}')".format(idx, shortenedURL, fullURL))
+	cur.execute("INSERT INTO urlShortener VALUES ({},'{}','{}', {})".format(idx, shortenedURL, fullURL, 0))
+	con.commit()
+
+def updateEntryCount(con, cur, column, columnNewValue, columnToCheck, columnToCheckValue):
+	cur.execute("UPDATE urlShortener SET {} = {} WHERE {} == '{}'".format(column, columnNewValue, columnToCheck, columnToCheckValue))
 	con.commit()
 
 def getLastIndex(cur):
